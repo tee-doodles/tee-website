3705a4bdbaabe0812717e74e709f4a83d2b20b16
tee-doodles <teegeetea@gmail.com>
2023-10-15 21:29:44 +0100

    added dialog placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArtComponent } from './art/art.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArtComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,10 +1,36 @@
+import { TmplAstRecursiveVisitor } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-art',
   templateUrl: './art.component.html',
-  styleUrls: ['./art.component.css']
+  styleUrls: ['./art.component.css'],
+  standalone: true,
+  imports: [MatExpansionModule, MatDialogModule],
 })
 export class ArtComponent {
 
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  
+  panelOpenState = false;
 }
+
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: './dialog-content-example-dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+})
+export class DialogContentExampleDialog {}
