9edece13b3d0eae3f430d4940928baf0f11ebcff
tee-doodles <teegeetea@gmail.com>
2023-10-16 16:39:47 +0100

    make dialogs funny

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -16,9 +17,10 @@ export class ArtComponent {
 
   constructor(public dialog: MatDialog) {}
 
-  openDialog() {
+  openDialog(value: Number) {
     const dialogRef = this.dialog.open(DialogContentExampleDialog);
-
+    dialogRef.componentInstance.name = value;
+    
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
@@ -30,7 +32,10 @@ export class ArtComponent {
 @Component({
   selector: 'dialog-content-example-dialog',
   templateUrl: './dialog-content-example-dialog.html',
+  styleUrls: ['./dialog-content.css'],
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, CommonModule],
 })
-export class DialogContentExampleDialog {}
+export class DialogContentExampleDialog {
+  name: Number = 0;
+}
